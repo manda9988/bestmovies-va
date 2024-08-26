bb442871607514f8fe717b105478d9d2677e3bcf
KAEL <vincent.achy@gmail.com>
2024-08-26 20:53:45 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
-import Image from "next/image";
+// page.tsx
+import {
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItemOption,
+  MenuOptionGroup,
+  Button,
+  VStack,
+} from "@chakra-ui/react";
+import { FaSearch, FaChevronDown } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Flex
+      direction="column"
+      alignItems="center"
+      padding="4"
+      bg="gray.800"
+      color="white"
+      minHeight="100vh"
+    >
+      <Heading as="h1" size="2xl" marginBottom="5" marginLeft="20px">
+        bestMovies
+      </Heading>
+      <InputGroup width={{ base: "85%", md: "60%" }} maxWidth="600px">
+        <Input
+          placeholder="Tapez votre recherche..."
+          bg="gray.700"
+          color="white"
+          borderRadius="md"
+          paddingRight="4rem"
         />
-      </div>
+        <InputRightElement>
+          <FaSearch color="white" />
+        </InputRightElement>
+      </InputGroup>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <VStack
+        spacing={4}
+        align="stretch"
+        width={{ base: "85%", md: "60%" }}
+        marginTop="6"
+      >
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={Button}
+            rightIcon={<FaChevronDown />}
+            colorScheme="gray"
+            variant="outline"
+          >
+            Par genres
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup title="Genres" type="checkbox">
+              <MenuItemOption value="action">Action</MenuItemOption>
+              <MenuItemOption value="comedy">Comedy</MenuItemOption>
+              <MenuItemOption value="drama">Drama</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={Button}
+            rightIcon={<FaChevronDown />}
+            colorScheme="gray"
+            variant="outline"
+          >
+            Par années de production
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup title="Années" type="checkbox">
+              <MenuItemOption value="2020">2020</MenuItemOption>
+              <MenuItemOption value="2021">2021</MenuItemOption>
+              <MenuItemOption value="2022">2022</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Menu closeOnSelect={false}>
+          <MenuButton
+            as={Button}
+            rightIcon={<FaChevronDown />}
+            colorScheme="gray"
+            variant="outline"
+          >
+            Par pays
+          </MenuButton>
+          <MenuList minWidth="240px">
+            <MenuOptionGroup title="Pays" type="checkbox">
+              <MenuItemOption value="us">États-Unis</MenuItemOption>
+              <MenuItemOption value="fr">France</MenuItemOption>
+              <MenuItemOption value="jp">Japon</MenuItemOption>
+            </MenuOptionGroup>
+          </MenuList>
+        </Menu>
+      </VStack>
+    </Flex>
   );
 }
