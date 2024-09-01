a3e8054a247702b6ff4f6ecc1db6eadd800719dc
KAEL <vincent.achy@gmail.com>
2024-09-01 20:49:33 +0200

    commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import { Heading } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Heading as="h1" size="2xl" marginBottom="5" marginLeft="20px">
-      bestMoviesss
+      bestMoviess
     </Heading>
   );
 }
