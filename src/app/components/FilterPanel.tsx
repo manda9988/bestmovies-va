8d01206784f5412a8237bb2cefd881936e9493d5
KAEL <vincent.achy@gmail.com>
2024-09-05 19:13:51 +0200

    commit

--- src/app/components/FilterPanel.tsx
+++ src/app/components/FilterPanel.tsx
@@ -23,9 +23,19 @@ export function FilterPanel() {
       <SingleFilter
         title="Par années de production"
         options={[
-          { value: "2020", label: "2020" },
-          { value: "2021", label: "2021" },
-          { value: "2022", label: "2022" },
+          { value: "2020-2029", label: "2020 - 2029" },
+          { value: "2010-2019", label: "2010 - 2019" },
+          { value: "2000-2009", label: "2000 - 2009" },
+          { value: "1990-1999", label: "1990 - 1999" },
+          { value: "1980-1989", label: "1980 - 1989" },
+          { value: "1970-1979", label: "1970 - 1979" },
+          { value: "1960-1969", label: "1960 - 1969" },
+          { value: "1950-1959", label: "1950 - 1959" },
+          { value: "1940-1949", label: "1940 - 1949" },
+          { value: "1930-1939", label: "1930 - 1939" },
+          { value: "1920-1929", label: "1920 - 1929" },
+          { value: "1910-1919", label: "1910 - 1919" },
+          { value: "1900-1909", label: "1900 - 1909" },
         ]}
       />
       <SingleFilter
