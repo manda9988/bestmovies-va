04fc5bae76e75fbe0f4c26ad9b2569b56b93b3d3
KAEL <vincent.achy@gmail.com>
2024-09-10 19:05:27 -0400

    commit

---
+++ src/app/components/moviesData copie.ts
@@ -0,0 +1,136 @@
+// src/app/components/moviesData.ts
+
+// export const movies = [
+//   {
+//     title: "Alien: Romulus",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien2",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/terminatorIMG.jpeg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien3",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien4",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien5",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien6",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien7",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien8",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien9",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien10",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien11",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+//   {
+//     title: "Alien12",
+//     releaseDate: "14 août 2024",
+//     duration: "1h 59min",
+//     genre: "Epouvante-horreur, Science Fiction",
+//     director: "Fede Alvarez",
+//     cast: "Cailee Spaeny, David Jonsson, Archie Renaux",
+//     description:
+//       "Alors qu’il entreprend des fouilles dans une station spatiale abandonnée, un groupe de jeunes voyageurs se retrouve confronté à la forme de vie la plus terrifiante de l'univers...",
+//     posterUrl: "/images/alienIMG.jpg", // Chemin mis à jour
+//   },
+// ];
