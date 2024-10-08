76adae4922cfd99e55ac7e91d1b3e6df1b8953a7
KAEL <vincent.achy@gmail.com>
2024-10-08 22:56:21 +0200

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ChakraProvider, Flex } from "@chakra-ui/react"; // Ajout de Flex
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,6 +10,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Findmovies",
   description: "A simple app to search for movies and series",
+  icons: {
+    icon: "/faviconLogo.png", // Ajout du chemin vers ton favicon dans le dossier public
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/faviconLogo.png" />{" "}
+        {/* Ajout explicite de la balise favicon */}
+      </head>
       <body className={inter.className}>
         <ChakraProvider>
           <Flex direction="column" minHeight="100vh">
