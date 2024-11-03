3b4759fb18301f255ac1b4f1aad6f583778db25b
KAEL <vincent.achy@gmail.com>
2024-11-03 23:24:10 +0100

    commit

--- src/app/components/MoviesList.tsx
+++ src/app/components/MoviesList.tsx
@@ -26,7 +26,7 @@ export default function MoviesList({ movies }: MoviesListProps) {
       alignItems="center"
       justifyContent="center"
       mt="1"
-      width={{ base: "90%", sm: "85%", md: "75%" }}
+      width={{ base: "90%", sm: "75%" }}
       mx="auto"
       mb="9"
     >
